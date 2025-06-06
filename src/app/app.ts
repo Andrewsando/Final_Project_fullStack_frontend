baecda36351f620c7cac235fdd79a9ebef95df4c
Andres <andres.torressandovalhotmail.com>
2025-06-05 19:52:34 -0500

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'Final_Project_fullStack_frontend';
+}
